b91d8db24c3120b610816582d1f7d9f95d10bf24
lyuhan-yeong <lyuhan-yeong@Hanyoungs-MacBook-Pro.local>
2024-11-19 20:01:34 -0500

    Level 1 Complete

---
+++ task1/using-js-to-make-changes.js
@@ -0,0 +1,11 @@
+const documentTitle =  document.querySelector("Head title")
+documentTitle.textContent = "Task 1 by [Your Name Here";
+
+const headerTitle = document.querySelector("header h1");
+headerTitle.textContent = "Task1";
+
+const description = document.querySelector("header p")
+description.textContent = "Wants to be an engineer"
+
+const listTitle = document.querySelector("section h2")
+listTitle.textContent = "List of things I like";
